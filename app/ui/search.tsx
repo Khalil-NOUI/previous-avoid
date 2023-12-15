1c3d3a4ad05199816ea499e92cd4042486970ef3
Khalil-NOUI <NOUI.KHALIL.PR@GMAIL.COM>
2023-12-16 00:56:14 +0100

    Fix Url Syn with Input

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -11,7 +11,8 @@ export default function Search({ placeholder }: { placeholder: string }) {
   const { replace } = useRouter();
 
   const handleSearch = useDebouncedCallback((term) => {
-    console.log(`Searching... ${term}`);
+    // console.log(`Searching... ${term}`);
+    // console.log(searchParams)
   const params = new URLSearchParams(searchParams);
     
     if (term) {
@@ -32,7 +33,7 @@ export default function Search({ placeholder }: { placeholder: string }) {
         placeholder={placeholder}
         onChange={(e) => {
           handleSearch(e.target.value)}}
-          
+          defaultValue={searchParams.get('query')?.toString()}
       />
       <MagnifyingGlassIcon className="absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
     </div>
