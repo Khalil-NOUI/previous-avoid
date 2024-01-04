ad78306bdac3a1213b1df88d72f2a53a3ef33df1
Khalil-NOUI <NOUI.KHALIL.PR@GMAIL.COM>
2024-01-04 22:50:37 +0100

    Search & Pagination

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -11,10 +11,11 @@ export default function Search({ placeholder }: { placeholder: string }) {
   const { replace } = useRouter();
 
   const handleSearch = useDebouncedCallback((term) => {
-    // console.log(`Searching... ${term}`);
-    // console.log(searchParams)
-  const params = new URLSearchParams(searchParams);
+    console.log(`Searching... ${term}`);
+    console.log(searchParams)
     
+  const params = new URLSearchParams(searchParams);
+    params.set('page', '1');
     if (term) {
       params.set('query', term);
     } else {
